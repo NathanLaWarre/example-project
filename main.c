8b272f952be4e07584f456ead5ec26eb0bd4ebc5
Nathan LaWarre <AIUGET3@amway.com>
2019-08-05 09:57:11 -0400

    initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int currentFeeling = 0, likeAPlasticBag = 0;
+string status;
+bool want, startAgain = true;
+
+if(currentFeeling == likeAPlasticBag){
+  status = "drifting through the wind";
+  want = startAgain;
+}
